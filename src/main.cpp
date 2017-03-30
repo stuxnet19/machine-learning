5d329b21478cabf617dd9c7ad0f9ed5af01c12d7
nbellach <nassim.bellache@etu.u-cergy.fr>
2017-03-30 15:49:30 +0200

    -->

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-#include "jpg.h"
+/*#include "jpg.h"
 #include "mnist.h"
 
 
@@ -19,3 +19,44 @@ int main(int argc, char** argv)
     }
     return 0;
 }
+*/
+
+#include "jpg.h"
+#include "mnist.h"
+
+float dist(float* v1, float* v2){
+	int i;
+	float d=0;
+	for (i=0; i<784; i++){
+		d+= (v1[i]-v2[i])*(v1[i]-v2[i]);		
+	}
+	return d;
+}
+
+
+int main(){
+	float** images = read_mnist("train-images.idx3-ubyte");
+	float* labels = read_labels("train-labels.idx1-ubyte");
+	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+
+    for(int i=0; i<10000; i++) {
+		printf("%u\n", i);
+		float mind= -1;
+		int NN;
+		for (int j=0; j<60000; j++){
+			float d = dist(test_images[i], images[j]);
+			if (d<mind || mind==-1){
+				mind = d;
+				NN = j;
+			}
+		}int inference = labels[NN];
+		save_jpeg(test_images[i], 28, 28, "%u/%u.jpg", inference, i);
+	}
+	
+    return 0;
+}
+
+
+
+
+
