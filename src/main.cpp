aa47ac5d23b102b9305cde14531f3b165d5e1ca3
nbellach <nassim.bellache@etu.u-cergy.fr>
2017-03-30 16:47:43 +0200

    -->

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,3 @@
-
-/*#include "jpg.h"
-#include "mnist.h"
-
-
-
-int main(int argc, char** argv)
-{
-    if(argc < 3) {fprintf(stderr, "Please provide two a mnist file\n"); exit(1); }
-    string path = argv[1];
-		string path_labels = argv[2]
-
-    float** data = read_mnist(path);
-	float* labels = read_labels(path-labels);
-
-    for(int i=0; i<60000; i++) {
-        printf("%u\n", i, (int)labels[i]);
-        save_jpg(data[i], 28, 28, "%u/%04u.jpg", (int)labels[i], i/1000, i%1000);
-    }
-    return 0;
-}
-*/
-
 #include "jpg.h"
 #include "mnist.h"
 
